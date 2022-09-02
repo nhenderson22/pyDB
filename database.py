16304e93da23577bb64d2dc91d16935c0ef35bea
Noah Henderson <noahscothenderson@gmail.com>
2022-09-01 23:03:01 -0600

    Cleaned up some methods and error handling

--- database.py
+++ database.py
@@ -5,22 +5,24 @@ import json
 class DataBase():
     def __init__(self, path):
         self.path = path
-        self.current = open(self.path)
-    #initializes a new table in the database 
-    def addData(self,data):
-       temp = json.dumps(data)
-       print(temp)
-       with open(self.path, 'w') as f:
-           f.write(temp)
+        if os.stat(self.path).st_size != 0:
+            with open(self.path) as f:
+                self.current = json.load(f)
+        else:
+            self.current = {}
 # Creates a table that can be added to our database
     def createTable(self,key,data):
         x = {str(key): data}
-        jsonObj = json.dumps(x)
-        return jsonObj
-    
-    def write(self,jsonObj):
+        self.current.update(x)
+# Writes to the database    
+    def write(self):
         with open(self.path, 'w') as f:
-            f.write(jsonObj)
-
-    def removeData(key):
-        pass
+            f.write(json.dumps(self.current))
+# Converts the working dictionary to python    
+    def convertToJson(self):
+        jsonObj = json.dumps(self.current)
+        return jsonObj
+# Used to update a tables data (can only handle a table)
+    def updateData(self, key, newData):
+        self.current[key] = newData
+     
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from database import DataBase
 
 db = DataBase('./test.json')
-db.addData({'tes2t':'Tesst1'})
+db.createTable('students', {'username':'nhenderson22'})
+db.write()
