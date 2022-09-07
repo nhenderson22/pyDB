49d96540f2e057cb568aec8452e7994fe7fbff6a
Noah Henderson <noahscothenderson@gmail.com>
2022-09-06 21:32:39 -0600

    Basic Shell implemented and errors handled

--- database.py
+++ database.py
@@ -1,14 +1,20 @@
 import os
 import json
+from os.path import exists
 
 class DataBase():
     def __init__(self, path):
         self.path = path
-        if os.stat(self.path).st_size != 0:
-            with open(self.path) as f:
-                self.current = json.load(f)
-        else:
-            self.current = {}
+        self.exists = True
+        try:
+            if os.stat(self.path).st_size != 0:
+                with open(self.path) as f:
+                    self.current = json.load(f)
+            else:
+                self.current = {}
+        except:
+            print("File not found")
+            self.exists = False
 # Creates a table that can be added to our database
     def createTable(self,key,data):
         x = {str(key): data}
@@ -17,11 +23,8 @@ class DataBase():
     def write(self):
         with open(self.path, 'w') as f:
             f.write(json.dumps(self.current))
-# Converts the working dictionary to python    
-    def convertToJson(self):
-        jsonObj = json.dumps(self.current)
-        return jsonObj
-    
+# Useful for my shell and CLI its probably easier to just use dictionary keys
+# Readability may be a factor I guess
     def getTable(self, key):
         return self.current[key]
 
--- shell.py
+++ shell.py
@@ -5,16 +5,19 @@ dbase = input("Please input patch to database: ")
 db = DataBase(dbase)
 running = True
 
-while running:
+while running and db.exists:
     x=input("Command:")
     if x == "getTable":
         y = input("Key:")
-        db.getTable(y)
+        print(db.getTable(y))
     elif x == "createTable":
         y = input("key: ")
         z = json.loads(input("Dictionary: "))
         db.createTable(y,z)
         db.write()
+    elif x == "save":
+        db.write()
     elif x == "quit":
         running = False
-    
+    else:
+        print("Command not found")
